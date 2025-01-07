81b30840b4461fa33e4de5a36f3b5befa7915fbe
Sergiy Umanets <umserg@gmail.com>
2025-01-07 10:26:27 +0200

    Refactor: Extracted methods appendSeparator, adjustColumnWidth, and appendFormattedLine

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.text.*;
 
 /** Containing items and calculating price. */
-public class ShoppingCart{
+public class ShoppingCart {
 
     public static enum ItemType { NEW, REGULAR, SECOND_FREE, SALE };
-    
+
     private List<Item> items = new ArrayList<Item>();
-   
+
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
         cart.addItem("Apple", 0.99, 5, ItemType.NEW);
@@ -25,8 +25,8 @@ public class ShoppingCart{
         if (quantity <= 0)
             throw new IllegalArgumentException("Illegal quantity");
         Item item = new Item();
-        item.title  = title;
-        item.price  = price;
+        item.title = title;
+        item.price = price;
         item.quantity = quantity;
         item.type = type;
         items.add(item);
@@ -39,7 +39,7 @@ public class ShoppingCart{
         String[] header = {"#","Item","Price","Quan.","Discount","Total"};
         int[] align = new int[] { 1, -1, 1, 1, 1, 1 };
         double total = 0.00;
-        int  index = 0;
+        int index = 0;
         for (Item item : items) {
             int discount = calculateDiscount(item.type, item.quantity);
             double itemTotal = item.price * item.quantity * (100.00 - discount) / 100.00;
@@ -56,43 +56,52 @@ public class ShoppingCart{
         String[] footer = { String.valueOf(index),"","","","",
                 MONEY.format(total) };
         int[] width = new int[]{0,0,0,0,0,0};
-        for (String[] line : lines)
-            for (int i = 0; i < line.length; i++)
-                width[i] = (int) Math.max(width[i], line[i].length());
-            for (int i = 0; i < header.length; i++)
-                width[i] = (int) Math.max(width[i], header[i].length());
-            for (int i = 0; i < footer.length; i++)
-                width[i] = (int) Math.max(width[i], footer[i].length());
-
+        for (String[] line : lines) {
+            adjustColumnWidth(width, line);
+        }
+        adjustColumnWidth(width, header);
+        adjustColumnWidth(width, footer);
+        
         int lineLength = width.length - 1;
         for (int w : width)
             lineLength += w;
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < header.length; i++)
-            appendFormatted(sb, header[i], align[i], width[i]);
-        sb.append("\n");
-
-        for (int i = 0; i < lineLength; i++)
-           sb.append("-");
-        sb.append("\n");
+        appendFormattedLine(sb, header, align, width, true);
+        appendSeparator(sb, lineLength);
                                  
         for (String[] line : lines) {
-            for (int i = 0; i < line.length; i++)
-                appendFormatted(sb, line[i], align[i], width[i]);
-            sb.append("\n");
+            appendFormattedLine(sb, line, align, width, true);
         }
 
         if (lines.size() > 0) {
-            for (int i = 0; i < lineLength; i++)
-                sb.append("-");
-            sb.append("\n");
+            appendSeparator(sb, lineLength);
         }
                                    
-
-        for (int i = 0; i < footer.length; i++)
-            appendFormatted(sb, footer[i], align[i], width[i]);
+        appendFormattedLine(sb, footer, align, width, false);
         return sb.toString();
+    }
+
+    private void appendSeparator(StringBuilder sb, int lineLength){ 
+        for(int i = 0; i < lineLength; i++)
+            sb.append("-");
+        sb.append("\n");
+    }
+
+    private void adjustColumnWidth(int[] width, String[] columns){ 
+        for(int i = 0; i < width.length; i++)
+            width[i] = (int) Math.max(width[i], columns[i].length());
+    }
+
+    private void appendFormattedLine(StringBuilder sb, 
+                                     String[] line, 
+                                     int[] align, 
+                                     int[] width, 
+                                     boolean newLine){
+        for(int i = 0; i < line.length; i++) 
+            appendFormatted(sb, line[i], align[i], width[i]);
+        if(newLine) 
+            sb.append("\n");
     }
 
     private static final NumberFormat MONEY;
@@ -100,12 +109,11 @@ public class ShoppingCart{
         DecimalFormatSymbols symbols = new DecimalFormatSymbols();
         symbols.setDecimalSeparator('.');
         MONEY = new DecimalFormat("$#.00", symbols);
-
     }
 
     public static void appendFormatted(StringBuilder sb, String value, int align, int width){
         if (value.length() > width)
-            value = value.substring(0,width);
+            value = value.substring(0, width);
         int before = (align == 0) 
                      ? (width - value.length()) / 2 
                      : (align == -1) ? 0 : width - value.length(); 
